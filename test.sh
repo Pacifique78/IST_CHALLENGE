4d56f771d55d0eae2043be3f10155ed00d5ed1b0
Pacifique <tuyipaccy78@gmail.com>
2024-11-07 03:35:53 +0200

    change the pipeline

--- test.sh
+++ test.sh
@@ -9,18 +9,18 @@ cd /app/python_app
 python3 -m pytest --cov=. --cov-report=xml --cov-report=term-missing -v
 echo "Python tests completed!"
 
-echo "2. Running Java tests..."
-cd /app/java_app
-# Run any necessary migrations
-./mvnw test
-echo "Java tests completed!"
+# echo "2. Running Java tests..."
+# cd /app/java_app
+# # Run any necessary migrations
+# ./mvnw test
+# echo "Java tests completed!"
 
-echo "3. Running Node.js tests..."
-cd /app/node_app
-# Run migrations if needed
-npm run test:migrate # (if you have such a script)
-./node_modules/.bin/jest --config jest.config.ts
-echo "Node.js tests completed!"
+# echo "3. Running Node.js tests..."
+# cd /app/node_app
+# # Run migrations if needed
+# npm run test:migrate # (if you have such a script)
+# ./node_modules/.bin/jest --config jest.config.ts
+# echo "Node.js tests completed!"
 
 echo "Running SonarQube analysis..."
 cd /app
